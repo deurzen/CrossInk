f6976e930c7190b05ae9be8fa557e19e677092de
deurzen <max@deurzen.net>
2026-07-10 14:49:00 +0200

    feat: add word inbox browsing storage API

--- src/word_inbox/WordInboxStore.h
+++ src/word_inbox/WordInboxStore.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <HalStorage.h>
+
+#include <cstddef>
 #include <cstdint>
+#include <string>
 #include <string_view>
 
 enum class WordInboxBookType : uint8_t {
@@ -35,7 +39,45 @@ struct WordInboxCapture {
   int displayHeight = 0;
 };
 
+struct WordInboxBookInfo {
+  char key[24] = {};
+  WordInboxBookType bookType = WordInboxBookType::Epub;
+  std::string title;
+  std::string author;
+  std::string bookPath;
+  uint32_t contextCount = 0;
+  uint32_t latestContextId = 0;
+};
+
+struct WordInboxContextInfo {
+  uint32_t id = 0;
+  uint32_t position = 0;
+  uint32_t contextCount = 0;
+  uint32_t previousId = 0;
+  uint32_t nextId = 0;
+  int32_t spineIndex = -1;
+  uint32_t currentPage = 0;
+  uint32_t totalPages = 0;
+  uint8_t progressPercent = 0;
+  bool hasText = false;
+  bool textTruncated = false;
+  bool hasScreenshot = false;
+  std::string chapterTitle;
+  uint32_t textLength = 0;
+  uint32_t textOffset = 0;
+};
+
+using WordInboxBookVisitor = bool (*)(void* context, const WordInboxBookInfo& book);
+
 class WordInboxStore {
  public:
   static WordInboxSaveResult save(const WordInboxCapture& capture, uint32_t& outCaptureId);
+
+  static bool isValidBookKey(std::string_view key);
+  static bool visitBooks(void* context, WordInboxBookVisitor visitor);
+  static bool getContext(std::string_view bookKey, uint32_t id, WordInboxContextInfo& out);
+  static bool openContextText(std::string_view bookKey, const WordInboxContextInfo& context, HalFile& outFile);
+  static bool getScreenshotPath(std::string_view bookKey, uint32_t id, char* output, size_t outputSize);
+  static bool deleteContext(std::string_view bookKey, uint32_t id);
+  static bool deleteBook(std::string_view bookKey);
 };
